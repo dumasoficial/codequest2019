3ec4310536563ff13d304a1a18c3dca0b8ffe09d
suddendropingravitas <kissthisthen42@gmail.com>
2019-05-03 19:57:46 -0700

    .

--- 20.py
+++ 20.py
@@ -9,22 +9,21 @@ for _ in range(cases):
     for _ in range(int(k)):
         name,length,width,wing,angle=sys.stdin.readline().split()
         length,width,wing,angle=float(length),float(width),float(wing),float(angle)
-        known.append([length,width,wing,angle])
         d[length,width,wing,angle]=name
     for _ in range(int(u)):
         unknown_data=[float(i) for i in sys.stdin.readline().split()]
         dists={}
         k=5
-        for data_point in known:
+        for data_point in d.keys():
             dist=sum([(i-j)**2 for i,j in zip(data_point,unknown_data)])**0.5
-            dists[dist]=tuple(data_point)
+            dists[dist]=data_point
             sorted_keys=sorted(list(dists.keys()))
         while True:
             k_nearest=sorted_keys[:k]
             a=sum([d[dists[i]]=='Accipitridae' for i in k_nearest])
             p=sum([d[dists[i]]=='Passeridae' for i in k_nearest])
             c=sum([d[dists[i]]=='Cathartidae' for i in k_nearest])
-            print(a,p,c)
+            #print(a,p,c)
             if a>p and a>c:
                 print('Accipitridae')
                 break
--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 Our solutions to <a href='https://www.lockheedmartin.com/en-us/who-we-are/communities/codequest.html'>Lockheed Martin's 2019 Code Quest coding competition. </a><br>
 <ul>
 <li>Because the official PDF with the problems hasn't been released I've included cached pages. </li>
-<li>This year input was read from "the standard input channel", not from a file. To do this in python you can use "sys.stdin.readline()".</li>
-<li>All solutions written in Python 3.5 </li>
-<li>Feel free to collaborate</li>
+<li>This year input was read from "the standard input channel", not from a file. To do this in Python you can use "sys.stdin.readline()".</li>
+<li>Feel free to contribute</li>
 </ul>
